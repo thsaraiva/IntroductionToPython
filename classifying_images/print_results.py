a1df6d8960dea100fb76a71205452639579f17c7
Thiago Saraiva <thsaraiva@gmail.com>
2020-04-05 20:13:51 +0100

    Printing results of image classifying program

--- classifying_images/print_results.py
+++ classifying_images/print_results.py
@@ -26,13 +26,14 @@
 #         This function does not output anything other than printing a summary
 #         of the final results.
 ##
-# TODO 6: Define print_results function below, specifically replace the None
-#       below by the function definition of the print_results function. 
-#       Notice that this function doesn't to return anything because it  
-#       prints a summary of the results using results_dic and results_stats_dic
-# 
-def print_results(results_dic, results_stats_dic, model, 
-                  print_incorrect_dogs = False, print_incorrect_breed = False):
+# Define print_results function below, specifically replace the None
+# below by the function definition of the print_results function.
+# Notice that this function doesn't to return anything because it
+# prints a summary of the results using results_dic and results_stats_dic
+
+
+def print_results(results_dic, results_stats_dic, model,
+                  print_incorrect_dogs=False, print_incorrect_breed=False):
     """
     Prints summary results on the classification and then prints incorrectly 
     classified dogs and incorrectly classified dog breeds if user indicates 
@@ -61,6 +62,35 @@ def print_results(results_dic, results_stats_dic, model,
                               False doesn't print anything(default) (bool) 
     Returns:
            None - simply printing results.
-    """    
-    None
-                
+    """
+    print("CNN model architectures: ", model)
+    print("    - Number of Images: ", results_stats_dic['n_images'])
+    print("    - Number of Dog Images: ", results_stats_dic['n_dogs_img'])
+    print("    - Number of \"Not-a\" Dog Images: ", results_stats_dic['n_not_dogs_img'])
+
+    for key, value in results_stats_dic.items():
+        if key.startswith('pct_'):
+            print("    - {}: {:5.1f}".format(key, value))
+
+    misclassified_dogs = get_misclassified_dogs(results_stats_dic)
+    if print_incorrect_dogs and misclassified_dogs > 0:
+        print("    - Misclassified dogs: ", misclassified_dogs)
+        for filename, results in results_dic.items():
+            if sum(results[3:]) == 1:
+                print("        - Real label : {}, Classified label: {}".format(results[0], results[1]))
+
+    misclassified_breeds = get_misclassified_breeds(results_stats_dic)
+    if print_incorrect_breed and misclassified_breeds != 0:
+        print("    - Misclassified breeds: ", misclassified_breeds)
+        for filename, results in results_dic.items():
+            if sum(results[3:]) == 2 and results[2] == 0:
+                print("        - Real label : {}, Classified label: {}".format(results[0], results[1]))
+
+
+def get_misclassified_dogs(results_stats_dic):
+    return results_stats_dic['n_images'] - (
+            results_stats_dic['n_correct_dogs'] + results_stats_dic['n_correct_not_dogs'])
+
+
+def get_misclassified_breeds(results_stats_dic):
+    return results_stats_dic['n_correct_dogs'] - results_stats_dic['n_correct_breed']
